65e389ca1b194a9498df4112e14ea75c308d9a36
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-29 19:52:28 +0300

    feat: create `UIButtons` and display `flags` as icons

--- fun-with-flags/fun-with-flags/ViewControllers/GameViewController.swift
+++ fun-with-flags/fun-with-flags/ViewControllers/GameViewController.swift
@@ -2,7 +2,32 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        configureView()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+    
+    func configureView() -> Void {
+        view.backgroundColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
+    }
+    
+    func askQuestion() -> Void {
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 }
