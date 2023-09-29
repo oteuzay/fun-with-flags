51fac474db18a2fce9304c2aca40830149b95e3b
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-09-29 19:54:50 +0300

    feat: :sparkles: add background image to `ViewController`

--- fun-with-flags/fun-with-flags/ViewControllers/ViewController.swift
+++ fun-with-flags/fun-with-flags/ViewControllers/ViewController.swift
@@ -4,5 +4,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    func configureView() -> Void {
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        
+        backgroundImage.image = UIImage(named: "BG.jpg")
+        backgroundImage.alpha = 0.1
+
+        self.view.addSubview(backgroundImage)
+        self.view.sendSubviewToBack(backgroundImage)
     }
 }
