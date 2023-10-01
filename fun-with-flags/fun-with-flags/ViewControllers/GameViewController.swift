54d99ba1004a7288b02e42f8cedbcc942a540371
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-10-01 15:49:47 +0300

    feat: :sparkles: add `buttonTapped` functionality and set it up

--- fun-with-flags/fun-with-flags/ViewControllers/GameViewController.swift
+++ fun-with-flags/fun-with-flags/ViewControllers/GameViewController.swift
@@ -11,7 +11,7 @@ class GameViewController: UIViewController {
     var score = 0
     var correctAnswer = 0 {
         didSet {
-            var country = countries[correctAnswer]
+            let country = countries[correctAnswer]
             title = country.count > 2 ? country.capitalized : country.uppercased()
         }
     }
@@ -22,14 +22,14 @@ class GameViewController: UIViewController {
         configureView()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        askQuestion()
+        askQuestion(action: nil)
     }
     
     func configureView() -> Void {
         view.backgroundColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
     }
     
-    func askQuestion() -> Void {
+    func askQuestion(action: UIAlertAction!) -> Void {
         countries.shuffle()
         
         correctAnswer = Int.random(in: 0...2)
@@ -37,5 +37,21 @@ class GameViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "🥳 Correct"
+            score += 1
+        } else {
+            title = "🙁 Wrong"
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
 }
